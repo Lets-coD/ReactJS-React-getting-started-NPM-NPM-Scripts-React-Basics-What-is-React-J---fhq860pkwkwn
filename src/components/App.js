4aa0efa671e37684cf2ec164839adf0c2b2ef997
shashank <shashank.srivastava5523@gmail.com>
2022-07-15 08:18:31 +0530

    1st commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,28 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
-const App = () => {
-  return (
-    <></>
-  )
+//const App = () => {
+//  return (
+//    <></>
+//  )
+//}
+
+import Slides from './Slides';
+
+const title = "Slideshow App";
+
+const App = ({slides}) =>  {
+
+    return (
+        <div>
+            <h1 header={title}/>
+            <div className="App">
+                <Slides  slides={slides}/>
+            </div>
+        </div>
+    );
 }
 
-
 export default App;
+
+//export default App;
--- src/index.js
+++ src/index.js
@@ -3,4 +3,27 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import slides from './data.js';
 
-// ReactDOM.render(<App slides={slides}/>, document.getElementById('root'));
+const slides1 = [
+    {
+      title: "Today's workout plan",
+      text: "We're gonna do 3 fundamental exercises.",
+    },
+    {
+      title: "First, 10 push-ups",
+      text: "Do 10 reps. Remember about full range of motion. Don't rush.",
+    },
+    {
+      title: "Next, 20 squats",
+      text: "Squats are important. Remember to keep your back straight.",
+    },
+    {
+      title: "Finally, 15 sit-ups",
+      text: "Slightly bend your knees. Remember about full range of motion.",
+    },
+    {
+      title: "Great job!",
+      text: "You made it, have a nice day and see you next time!",
+    },
+  ];
+
+ ReactDOM.render(<App slides={slides1}/>, document.getElementById('root'));
