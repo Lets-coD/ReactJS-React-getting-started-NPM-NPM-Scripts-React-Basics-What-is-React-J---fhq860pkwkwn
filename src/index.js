afc94d83f7efc999e020f0afc024bff0b1f1ca0e
shashank <shashank.srivastava5523@gmail.com>
2022-07-15 08:48:57 +0530

    2d commit

--- src/index.js
+++ src/index.js
@@ -3,27 +3,5 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import slides from './data.js';
 
-const slides1 = [
-    {
-      title: "Today's workout plan",
-      text: "We're gonna do 3 fundamental exercises.",
-    },
-    {
-      title: "First, 10 push-ups",
-      text: "Do 10 reps. Remember about full range of motion. Don't rush.",
-    },
-    {
-      title: "Next, 20 squats",
-      text: "Squats are important. Remember to keep your back straight.",
-    },
-    {
-      title: "Finally, 15 sit-ups",
-      text: "Slightly bend your knees. Remember about full range of motion.",
-    },
-    {
-      title: "Great job!",
-      text: "You made it, have a nice day and see you next time!",
-    },
-  ];
 
- ReactDOM.render(<App slides={slides1}/>, document.getElementById('root'));
+ ReactDOM.render(<App slides={slides}/>, document.getElementById('root'));
